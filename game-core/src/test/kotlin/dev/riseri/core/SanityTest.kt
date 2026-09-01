2f46f1dae6a40cfdb20bdab62c92344102f5848d
riseri <naraosu@gmail.com>
2026-08-31 23:40:26 -0700

    Set up project foundation

---
+++ game-core/src/test/kotlin/dev/riseri/core/SanityTest.kt
@@ -0,0 +1,12 @@
+package dev.riseri.core
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SanityTest {
+
+    @Test
+    fun `basic arithmetic works`() {
+        assertEquals(4, 2 + 2)
+    }
+}
